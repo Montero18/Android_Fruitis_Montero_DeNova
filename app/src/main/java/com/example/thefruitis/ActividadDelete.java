4ed0789a48b4b38cfcc8512668f7dd798aa802fb
denova13 <denova1317@gmail.com>
2022-01-13 12:22:32 +0100

    Commit Añadir Actividad Y Menu

---
+++ app/src/main/java/com/example/thefruitis/ActividadDelete.java
@@ -0,0 +1,14 @@
+package com.example.thefruitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_delete);
+    }
+}
--- app/src/main/java/com/example/thefruitis/MainActivity.java
+++ app/src/main/java/com/example/thefruitis/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.thefruitis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Intent i1 = new Intent(this,ActividadDelete.class);
+                startActivity(i1);
+                break;
+            case R.id.item2:
+                Intent i2 = new Intent(this,BaseDatos.class);
+                startActivity(i2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
